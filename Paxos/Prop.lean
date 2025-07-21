7163e5a3b5aedd8ee48ac90f963f3dec1a079f46
Ethan H <jujugregoryh@gmail.com>
2025-07-21 16:10:45 -0400

    Update Prop (MsgInv)
    1. Previous MsgInv was wrong: Wrong parenthesis in the 1b case. Wasn't spotted previously because
       it was never used in the proof.
    
    2. Added invariant in 1b case: if the rbal and rval are not both `some`,
    then they must all be none. This corresponds to maxVBal = -1 in the TLA
    proof.
    
    3. Changed b to some b in 2b case, since we chanegd the constructor for
       twob to take Option Ballot instead of Ballot.

--- Paxos/Prop.lean
+++ Paxos/Prop.lean
@@ -46,18 +46,17 @@ MsgInv defines properties satisfied by the contents of messages, for 1b, 2a, and
 -/
 def MsgInv : Prop :=
   ∀ m ∈ sent, match m with
-    | Message.oneb b maxVBal maxVal a =>
-      match maxVal with                             -- ! in the TLA proof this is matched with ballot number (-1) and otherwise. This could lead to some issues proving the inductiveness about Phase2a. At the very least, we need an extra lemma that says: "maxVBal is -1 iff maxVal is none."
-        | none => True
-        | some v₀ => VotedForIn sent a v₀ maxVBal   -- this only applies to 1b messages
+    | Message.oneb b maxVBal maxVal a => (
+      match maxVBal, maxVal with                             -- ! in the TLA proof this is matched with ballot number (-1) and otherwise. We match it with both, and added another invariant that is, when one of them is none, the other is also none.
+        | some rbal, some rval => VotedForIn sent a rval rbal
+        | option_rbal, option_rval => option_rbal = none ∧ option_rval = none)          -- ADDED in the Lean proof
       ∧
-      (∀ (b' : Ballot), ((b' ≥ maxVBal + 1) ∧ (b' ≤ b - 1)) → ¬ (∃ v : Value, VotedForIn sent a v b'))
-    | Message.twoa b v => (SafeAt sent Quorums v b) -- this only applies to 2a messages
-                          ∧ (∀ m2 ∈ sent,
-                                match m2 with
-                                | Message.twoa b' _ => (b' = b → m2 = m)
-                                | _                  => True)
-    | Message.twob b (some v) _ => Message.twoa b v ∈ sent
+      (∀ (b' : Ballot), ((b' ≥ maxVBal + (1: Nat)) ∧ (b' < b)) → ¬ (∃ v : Value, VotedForIn sent a v b')) -- This works even if `maxVBal` is none, because we defined `LE` for `Ballot` and `Option Ballot`, see Spec.lean
+    | Message.twoa b v =>   (SafeAt sent Quorums v b) -- this only applies to 2a messages
+                          ∧ (∀ m2 ∈ sent, match m2 with
+                              | Message.twoa b' _ => (b' = b → m2 = m)
+                              | _                  => True)
+    | Message.twob (some b) (some v) _ => Message.twoa b v ∈ sent
     | _ => True
 
 def Consistency : Prop :=
