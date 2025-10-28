e22f31eca153326dbc8e22f35964f1c1b7784c43
Ethan H <jujugregoryh@gmail.com>
2025-10-28 12:57:23 -0400

    format extra lemma and prop

--- Paxos/ExtraLemma.lean
+++ Paxos/ExtraLemma.lean
@@ -1,3 +1,7 @@
+/-
+  Lean translation of Paxos. Lemmas for making the proof easier later.
+  The lemmas here are not in TLA: https://arxiv.org/pdf/1802.09687
+-/
 import Paxos.Spec
 import Paxos.Prop
 import Mathlib.Tactic
@@ -8,31 +12,47 @@ open Paxos.Spec Paxos.Prop
 open Set Classical
 
 -- Re-introduce variables --
-variable (Quorums: Set (Set Acceptor)) -- Reintroduce Quorums
-variable (sent sent': Set Message) -- Reintroduce sent
+variable (Quorums: Set (Set Acceptor))
+variable (sent sent': Set Message)
 
-/-- The immediate consequence of `QuorumAssumption`.
-    It is used to pick a witness from the quorum intersection. -/
---! nonempty_iff_ne_empty took some time to find
+/-- Effort: 5m
+    Used in proving that Phase2a is inductive
+-/
 @[simp]
-lemma pick_from_quorum_int (h1: Q₁ ∈ Quorums)
-                           (h2: Q₂ ∈ Quorums): ∃a: Acceptor, a ∈ Q₁ ∩ Q₂ := by
+lemma ballot_none_plus_one_leq_ballot
+    {b : Ballot} : (none : Option Ballot) + (1 : Nat) ≤ some b := by
+  dsimp [HAdd.hAdd]
+  exact Nat.zero_le b
+
+/-- Effort: 10m (nonempty_iff_ne_empty took some time to find)
+    Used to pick a witness from the quorum intersection.
+    The immediate consequence of `QuorumAssumption`.
+-/
+@[simp]
+lemma pick_from_quorum_int {Q₁ Q₂ : Set Acceptor}
+    (h1: Q₁ ∈ Quorums)
+    (h2: Q₂ ∈ Quorums): ∃a: Acceptor, a ∈ Q₁ ∩ Q₂ := by
   have hne := QuorumAssumption Quorums h1 h2
   rcases ((Iff.mpr nonempty_iff_ne_empty) hne) with ⟨aa, haa⟩
   exact Exists.intro aa haa
 
-/-- Lemma for `Ballot` ordering, used in `ind_phase2a`. -/
+/-- Effort: 20m
+    Lemma for `Ballot` ordering, used in `ind_phase2a`.
+-/
 @[simp]
 lemma option.some_succ_le_some_of_some_le_and_lt {n m k : Ballot}
-  (h₁ : some n ≤ some m) (h₂ : m < k) : some (n + 1) ≤ some k := by
+    (h₁ : some n ≤ some m)
+    (h₂ : m < k) : some (n + 1) ≤ some k := by
   apply Option.some_le_some.mp at h₁
   apply le_trans (Nat.succ_le_succ h₁) (Nat.succ_le_iff.mpr h₂)
 
-/-- The messages in max_prop are of type `twob`. -/
+/-- Effort: 5m
+    The messages in max_prop are of type `twob`.
+-/
 @[simp]
 theorem mem_max_prop_is_twob {m : Message} {a : Acceptor}:
-          m ∈ max_prop sent a → ∃ (b : Option Ballot) (v : Option Value),
-            m = Message.twob b v a := by
+    m ∈ max_prop sent a → ∃ (b : Option Ballot) (v : Option Value),
+                            m = Message.twob b v a := by
   dsimp [max_prop] at *
   split_ifs with h_nonempty
   · simp
@@ -43,11 +63,12 @@ theorem mem_max_prop_is_twob {m : Message} {a : Acceptor}:
     rcases h_mem with ⟨_h_sent, ⟨b, v, rfl⟩, _⟩
     exact ⟨b, v, rfl⟩
 
-/-- If max_prop is not empty (`some v`),
-    then the acceptor must has voted for that value `v`.-/
+/-- Effort: 10m
+    If max_prop is not empty, then the acceptor must has voted for the value.
+-/
 @[simp]
 theorem max_prop_not_empty_implies_voted_for {a : Acceptor} {b : Ballot} {v : Value} :
-          Message.twob b (some v) a ∈ max_prop sent a → VotedForIn sent a v b := by
+    Message.twob b (some v) a ∈ max_prop sent a → VotedForIn sent a v b := by
   intro h_mem
   unfold max_prop at h_mem
   simp at h_mem
@@ -58,23 +79,27 @@ theorem max_prop_not_empty_implies_voted_for {a : Acceptor} {b : Ballot} {v : Va
   · exfalso
     simp at h_mem
 
--- Effort: 20 min
-/-- This lemma states that either one of ballot or value is empty, then the other one must also be empty.
-    This is used in later proof when we need to do pattern matching on both ballot and value for `Message.twob`,
-    and since they are both `Option`, we need to cover the following cases:
-    `match rbal, rval with | some rbal, none | none, some rval => ...`
-    This lemma allows us to eliminate these two cases. -/
+/-- Effort: 20m
+    This lemma states that either one of ballot or value is empty, then the other one
+    must also be empty.
+    This is used in later proof when we need to do pattern matching on both ballot and
+    value for `Message.twob`, and since they are both `Option`, we need to cover the
+    following cases: `match rbal, rval with | some rbal, none | none, some rval => ...`
+    This lemma allows us to eliminate these two cases.
+-/
 @[simp]
 lemma max_prop_empty_ballot_iff_empty_value {a : Acceptor} {m : Message}
-        (hm : m ∈ max_prop sent a) :
-        (∃ b, m = Message.twob (some b) none a) ↔ ∃ v, m = Message.twob none (some v) a := by
+    (hm : m ∈ max_prop sent a) :
+    (∃ b, m = Message.twob (some b) none a) ↔ ∃ v, m = Message.twob none (some v) a := by
   dsimp [max_prop] at hm
   let twobs := { m | m ∈ sent ∧ ∃ b v, m = Message.twob (some b) (some v) a }
   split_ifs at hm with h_nonempty
   · simp at hm; simp [hm];
   · simp [*] at hm; have h_m_twob := hm.left.right; rcases h_m_twob with ⟨b, v, rfl⟩; simp;
 
-/-- The acceptor could not have voted for a ballot greater than the one in `max_prop` -/
+/-- Effort: 20m
+    The acceptor could not have voted for a ballot greater than the one in `max_prop`
+-/
 @[simp]
 theorem max_prop_implies_not_voted_for_greater_ballots {a : Acceptor} {b : Ballot} {v: Value} :
           Message.twob b (some v) a ∈ max_prop sent a →
@@ -96,13 +121,15 @@ theorem max_prop_implies_not_voted_for_greater_ballots {a : Acceptor} {b : Ballo
   · exfalso
     simp at *
 
--- Effort: 20 min
-/-- If the message from `max_prop` has empty ballot and value, then the acceptor has not
-voted in any previous ballots. Used in proving the case when both `rbal` and `rval` are empty in the proof of `Phase1b`. -/
+/-- Effort: 20m
+    If the message from `max_prop` has empty ballot and value, then the acceptor has not
+    voted in any previous ballots. Used in proving the case when both `rbal` and `rval`
+    are empty in the proof of `Phase1b`.
+-/
 @[simp]
 theorem max_prop_empty_implies_not_voted_in_prev_ballots {a: Acceptor}
-          (hm: Message.twob none none a ∈ max_prop sent a) :
-            ∀ b, ∀ (v: Value), ¬ VotedForIn sent a v b := by
+    (hm: Message.twob none none a ∈ max_prop sent a) :
+    ∀ b, ∀ (v: Value), ¬ VotedForIn sent a v b := by
   intro b' hb'_le h_voted
   unfold max_prop at hm
   simp [*] at *
@@ -121,20 +148,26 @@ theorem max_prop_empty_implies_not_voted_in_prev_ballots {a: Acceptor}
 
 -- The following 5 lemmas are used to show that `sent` grows monotonically with `Next`. --
 
-/-- This is the immediate consequence of the definition of `Send`.
-    We use this lemma to show that `Next` grows the history variable `sent` monotonically.-/
+/-- Effort: 5m
+    This is the immediate consequence of the definition of `Send`.
+    We use this lemma to show that `Next` grows the history variable `sent` monotonically.
+-/
 @[simp]
 lemma send_monotonic {sent sent': Set Message} {x: Message}
                      (h: sent' = Send x sent) : sent ⊆ sent' := by
   unfold Send at h; intro X hX; rw [h]; exact mem_union_left {x} hX
 
--- `Phase1a` grows `sent` monotonically.
+/-- Effort: none (direct from definition)
+    `Phase1a` grows `sent` monotonically.
+-/
 @[simp]
 lemma phase1a_imp_mono_sent {b: Ballot}
                             (hPhase1a: Phase1a sent sent' b) : sent ⊆ sent' := by
   unfold Phase1a at hPhase1a; exact send_monotonic hPhase1a
 
--- `Phase1b` grows `sent` monotonically.
+/-- Effort: 5m
+    `Phase1b` grows `sent` monotonically.
+-/
 @[simp]
 lemma phase1b_imp_mono_sent {a: Acceptor}
                             (hPhase1b: Phase1b sent sent' a) : sent ⊆ sent' := by
@@ -149,14 +182,18 @@ lemma phase1b_imp_mono_sent {a: Acceptor}
     | _ => simp at hmatch;
   | _ => simp at hmatch;
 
--- `Phase2a` grows `sent` monotonically.
+/-- Effort: 5m
+    `Phase2a` grows `sent` monotonically.
+-/
 @[simp]
 lemma phase2a_imp_mono_sent {b: Ballot}
                             (hPhase2a: Phase2a Quorums sent sent' b) : sent ⊆ sent' := by
   unfold Phase2a at hPhase2a
   split_ifs at hPhase2a with h1 h2 <;> simp [hPhase2a]
 
--- `Phase2b` grows `sent` monotonically.
+/-- Effort: 5m
+    `Phase2b` grows `sent` monotonically.
+-/
 @[simp]
 lemma phase2b_imp_mono_sent {a: Acceptor}
                             (hPhase2b: Phase2b sent sent' a) : sent ⊆ sent' := by
@@ -168,7 +205,8 @@ lemma phase2b_imp_mono_sent {a: Acceptor}
     split_ifs at hmatch with hpos <;> simp [hmatch]
   | _ => simp [hmatch];
 
-/-- `sent` grows monotonically with `Next`.
+/--  Effort: 3m
+    `sent` grows monotonically with `Next`.
     This is used in the proof of `SafeAtStable`. -/
 @[simp]
 lemma next_imp_mono_sent (hNext: Next Quorums sent sent') : sent ⊆ sent' := by
@@ -179,42 +217,53 @@ lemma next_imp_mono_sent (hNext: Next Quorums sent sent') : sent ⊆ sent' := by
   · exact phase1b_imp_mono_sent sent sent' hPhase1b
   · exact phase2b_imp_mono_sent sent sent' hPhase2b
 
-/-- If `sent` is a subset of `sent'`, then `VotedForIn sent a v b` implies `VotedForIn sent' a v b`.
-    This is used in the proof of `SafeAtStable`. -/
+/-- Effort: 5m
+    If `sent` is a subset of `sent'`,
+    then `VotedForIn sent a v b` implies `VotedForIn sent' a v b`.
+    This is used in the proof of `SafeAtStable`.
+-/
 @[simp]
-lemma votedForIn_monotonic (h1: sent ⊆ sent'):
-                            VotedForIn sent a v b → VotedForIn sent' a v b := by
+lemma votedForIn_monotonic (h1: sent ⊆ sent') :
+    VotedForIn sent a v b → VotedForIn sent' a v b := by
   intro h1
   rcases h1 with ⟨m, hm, hmatch⟩
   use m
   refine (and_iff_right ?h.ha).mpr hmatch
   apply h1; exact hm
 
-/-- The immediate consequence of the definition of VotedForIn.
-    If no 2b message is added to `sent`, then the value predicate `¬VotedForIn` is preserved. -/
+/-- Effort: 15m
+    If no 2b message is added to `sent`,
+    then the value predicate `¬VotedForIn` is preserved.
+-/
 @[simp]
 lemma send_add_non_twob_preserves_no_vote {a: Acceptor} {b: Ballot} {m: Message}
-        (hnv : ∀ v, ¬ VotedForIn sent a v b)
-        (hsend : sent' = Send m sent)
-        (hm : (∃ bal, m = Message.onea bal) ∨
-              (∃ bal maxV maxVal a', m = Message.oneb bal maxV maxVal a') ∨
-              (∃ bal val, m = Message.twoa bal val)) : ∀ v, ¬ VotedForIn sent' a v b := by
+    (hnv : ∀ v, ¬ VotedForIn sent a v b)
+    (hsend : sent' = Send m sent)
+    (hm :   (∃ bal, m = Message.onea bal)
+          ∨ (∃ bal maxV maxVal a', m = Message.oneb bal maxV maxVal a')
+          ∨ (∃ bal val, m = Message.twoa bal val)) : ∀ v, ¬ VotedForIn sent' a v b := by
   intro v hVoted
   cases hm with
-  | inl h_1a => specialize hnv v; unfold VotedForIn at *; simp [hsend] at hVoted; cases hVoted with
-    | inl h_m_eq_2b => cases m with
-      | _ => simp [*] at *
-    | inr h_2b_in_sent => simp [h_2b_in_sent] at hnv;
+  | inl h_1a => specialize hnv v; unfold VotedForIn at *
+                simp [hsend] at hVoted
+                cases hVoted with
+                | inl h_m_eq_2b => cases m <;> simp [*] at *
+                | inr h_2b_in_sent => simp [h_2b_in_sent] at hnv;
   | inr h_1b_2a => cases h_1b_2a with
-    -- This could be simplified to one line with <;> as below, here keep it for clarity
-    | inl h_1b => specialize hnv v; unfold VotedForIn at *; simp [hsend] at hVoted; cases hVoted with
-      | inl h_m_eq_2b => cases m <;> simp [*] at *
-      | inr h_2b_in_sent => simp [h_2b_in_sent] at hnv;
-    | inr h_2a => specialize hnv v; unfold VotedForIn at *; simp [hsend] at hVoted; cases hVoted <;> cases m <;> simp [*] at *
+                   | inl h_1b =>  specialize hnv v
+                                  unfold VotedForIn at *
+                                  simp [hsend] at hVoted
+                                  cases hVoted <;> cases m <;> simp [*] at *
+                   | inr h_2a =>  specialize hnv v
+                                  unfold VotedForIn at *
+                                  simp [hsend] at hVoted
+                                  cases hVoted <;> cases m <;> simp [*] at *
 
-/-- Corresponds to the monotonicity of existantial quantifier describe in the TLAPS proof.
+/-- Effort: 5m
+    Corresponds to the monotonicity of existantial quantifier describe in the TLAPS proof.
     We combine this lemma with previous monotonic results to show some property `P` of
-    the history variable holds inductively. -/
+    the history variable holds inductively.
+-/
 theorem exists_mem_of_subset {s t : Set Message} {P : Message → Prop}
   (hsub : s ⊆ t) (hex : ∃ x ∈ s, P x) : ∃ x ∈ t, P x := by
   rcases hex with ⟨x, hx_s, hx_P⟩
--- Paxos/Prop.lean
+++ Paxos/Prop.lean
@@ -1,26 +1,30 @@
+/-
+  Lean translation of Paxos. This is for the propositions.
+  See the TLA equivalent in https://arxiv.org/pdf/1802.09687, Appendix B.
+  --! denotes the difference this specificaiton and the TLA spec.
+-/
 import Paxos.Spec
 
 namespace Paxos.Prop
 open Paxos.Spec
 
+--! Reintroduce variables, needed in Lean.
 variable (sent sent' : Set Message)
 variable (Quorums : Set (Set Acceptor))
 
-/-- VotedForIn(a, v, b) means that acceptor a has sent some 2b message m with m.val
-equal to v and m.bal equal to b. This specifies that acceptor a has voted the pair ⟨v, b⟩.-/
+/-- Same as in TLA. -/
 def VotedForIn (a : Acceptor) (v : Value) (b : Ballot) : Prop :=
   ∃ m ∈ sent, m = Message.twob b v a
 
-/-- ChosenIn(v, b) means that every acceptor in some quorum Q has voted the pair hv, bi. -/
+/-- Same as in TLA. -/
 def ChosenIn (v: Value) (b: Ballot) : Prop :=
   ∃ Q ∈ Quorums, ∀ a ∈ Q, VotedForIn sent a v b
 
-/-- Chosen(v) means that for some ballot b, ChosenIn(v, b) holds. -/
+/-- Same as in TLA. -/
 def Chosen (v : Value) : Prop :=
   ∃ b : Ballot, ChosenIn sent Quorums v b
 
-/-- WontVoteIn(a, b) means that acceptor a has seen a higher ballot than b,
-and did not and will not vote any value with ballot b. -/
+/-- Same as in TLA. Except for pattern matching. -/
 def WontVoteIn (a : Acceptor) (b : Ballot) : Prop :=
      (∀ v : Value, ¬VotedForIn sent a v b)
    ∧ (∃ m ∈ sent, match m with
@@ -28,19 +32,19 @@ def WontVoteIn (a : Acceptor) (b : Ballot) : Prop :=
                   | Message.twob b' _ a'   => a' = a ∧ b' > b
                   | _                      => false)
 
-/-- SafeAt(v, b) means that no value except perhaps v has been or will be chosen
-in any ballot lower than b. -/
+/-- Same as in TLA. -/
 def SafeAt (v : Value) (b : Ballot) : Prop :=
-  ∀ b2 < b,
-    ∃ Q ∈ Quorums,
-      ∀ a ∈ Q, VotedForIn sent a v b2 ∨ WontVoteIn sent a b2
+  ∀ b2 < b, ∃ Q ∈ Quorums, ∀ a ∈ Q, VotedForIn sent a v b2 ∨ WontVoteIn sent a b2
 
 /-- Message invariants for 1b, 2a, and 2b messages. -/
+--! The added case below is because we define `maxVBal` and `maxVal` to be `Option` type.
+--! Therefore, we need to say if they are not both `some …`, then they must be `none`.
 def MsgInv : Prop :=
   ∀ m ∈ sent, match m with
     | Message.oneb b maxVBal maxVal a  =>
         (match maxVBal, maxVal with
           | some rbal, some rval         => VotedForIn sent a rval rbal
+          --! The following case is added.
           | option_rbal, option_rval     => option_rbal = none ∧ option_rval = none)
       ∧ (∀ (b' : Ballot), (b' ≥ (maxVBal + (1: Nat)) ∧ (b' < b))
           → ¬(∃ v : Value, VotedForIn sent a v b'))
@@ -49,9 +53,9 @@ def MsgInv : Prop :=
                                              | Message.twoa b' _ => (b' = b → m2 = m)
                                              | _                 => True)
     | Message.twob (some b) (some v) _ =>   Message.twoa b v ∈ sent
-    | _ => True
+    | _ => True -- don't care about 1a messages.
 
-def Consistency : Prop :=
+def Agree : Prop :=
   ∀ (v1 v2 : Value), Chosen sent Quorums v1 ∧ Chosen sent Quorums v2 → v1 = v2
 
 end Paxos.Prop
